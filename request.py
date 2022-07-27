bcd54a805f6ff223b4a29d127907afe40ea20465
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-07-27 09:14:44 +0530

    fix requests not sending content-type header and minor refactor

--- request.py
+++ request.py
@@ -1,19 +1,21 @@
-import requests
+#!/usr/bin/env python3
+
 import json
 import time
 
-with open('mock_data/data.txt', 'r') as f:
-    data = '[' + f.read()[:-1] + ']'
-    data = json.loads(data)
+import requests
+
+with open('mock_data/data.txt') as f:
+    data = json.load(f)
 
 API_ENDPOINT = "http://127.0.0.1:8090/update_observations"
 
 while True:
     for req_data in data:
         try:
-            print (len(req_data), flush=True)
-            r = requests.post(url=API_ENDPOINT, data=json.dumps(req_data))
-            print("The response is: %s" % r.text)
+            print(len(req_data), flush=True)
+            r = requests.post(url=API_ENDPOINT, json=req_data)
+            print(f"The response is: {r.text}")
         except Exception as e:
             print(e)
         time.sleep(4)
